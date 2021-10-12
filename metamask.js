5d6eae4ab406fb0315d209029b08e6213c9c3498
DesignIsOrion <DesignIsOrion@gmail.com>
2021-10-12 06:48:59 -0400

    adding files

---
+++ metamask.js
@@ -0,0 +1,20 @@
+// connect to Moralis server
+Moralis.initialize("#"); // App ID
+Moralis.serverURL = "#"; // server url
+
+// add from here down
+async function login() {
+  let user = Moralis.User.current();
+  if (!user) {
+    user = await Moralis.authenticate();
+  }
+  console.log("logged in user:", user);
+}
+
+async function logOut() {
+  await Moralis.User.logOut();
+  console.log("logged out");
+}
+
+document.getElementById("btn-login").onclick = login;
+document.getElementById("btn-logout").onclick = logOut;
